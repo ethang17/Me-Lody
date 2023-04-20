b984512d54a22c877da42e92a71c20708a00d8a4
Jason <jkn32@drexel.edu>
2023-04-20 14:09:16 -0400

    Fixed the bad buttons

--- screens/SettingsScreen.tsx
+++ screens/SettingsScreen.tsx
@@ -8,7 +8,7 @@ export default function SettingsScreen({ navigation }: RootStackScreenProps<'Set
     <View style={styles.container}>
       <Text style={styles.title}>Settings</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <Text>This is where your settings tab will be</Text>
+      <Text>Settings tab will be right here</Text>
 
       {/* Use a light status bar on iOS to account for the black space above the modal */}
       <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
