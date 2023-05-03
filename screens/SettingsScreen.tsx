10865a3f375f67bf8f36ea14f59fdc3a71fd99e7
as5752 <Alex.Spetseris@drexel.edu>
2023-05-03 14:06:44 -0400

    Privacy Terms Pressable now fits text

--- screens/SettingsScreen.tsx
+++ screens/SettingsScreen.tsx
@@ -48,7 +48,7 @@ const styles = StyleSheet.create({
   },
   blockButton: {
     height: 50,
-    width: 100,
+    width: 150,
     backgroundColor: '#DDD',
     alignItems: 'center',
     paddingVertical: 12,
