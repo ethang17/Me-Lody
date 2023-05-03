237adda85047647f3420d2e3d3117001f8b5f000
Ethan Grim <ethangrimmy33@gmail.com>
2023-05-03 19:24:02 -0400

    Scrollable View

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Pressable, Image } from 'react-native';
+import { StyleSheet, Pressable, Image, ScrollView } from 'react-native';
 import { useState } from 'react';
 import { getKeys } from '../components/makeAccount';
 import EditScreenInfo from '../components/EditScreenInfo';
@@ -7,15 +7,12 @@ import { RootTabScreenProps } from '../types';
 import { useRoute } from '@react-navigation/native';
 import { getUserPost, getSaved } from './PostScreen';
 
-let signedIn = false
+let signedIn = true
 export function setSignedIn(bool: boolean){
   signedIn = bool
 }
 let posts: JSX.Element[] = []
 
-function setUser(post: JSX.Element){
-  
-}
 export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
   const route = useRoute();
   posts[0] = (
@@ -28,7 +25,7 @@ export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'
   }
   if (signedIn == true) {
     return (
-
+      <ScrollView>
       <View style={styles.container}>
         <Text style={styles.title}>Me-Lody</Text>
         <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
@@ -39,6 +36,7 @@ export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'
       <Text style={styles.otherText}> Click me to post a song!</Text></Pressable>
       <Image source={require('../assets/images/blond.png')} />
       </View>
+      </ScrollView>
     );
   }
   else {
@@ -55,10 +53,11 @@ export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: 'rgb(204, 197, 244 )'
+
+    backgroundColor: 'rgb(204, 197, 244 )',
+    height: 2000,
+
   },
   postHolder:{
     alignItems: 'center',
@@ -68,10 +67,11 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 20,
     fontWeight: 'bold',
-    color: 'rgb(41, 41, 95 )'
+    color: 'rgb(41, 41, 95 )',
+    marginTop:100,
   },
   otherText:{
-    fontsize: 12,
+    fontSize: 12,
     fontWeight: 'bold',
     color: 'rgb(41, 41, 95 )'
   },
